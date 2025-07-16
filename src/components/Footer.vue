2ec86d2d8b0648ad5fa0b8538cd4a524cd8187e8
Isjustmy <ifpersona205@gmail.com>
2025-07-16 18:31:11 +0700

    fix: logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,10 +11,10 @@
         <a class="ml-2" href="https://youtube.com/@if_inyoutube205" target="_blank">
           <img id="youtubeIcon" type="image/svg+xml" src="../assets/youtube.svg" />
         </a>
-        <a class="ml-2" href="https://linkedin.com/in/ibnu-fajar-isjustmy" target="_blank">
+        <a class="ml-4" href="https://linkedin.com/in/ibnu-fajar-isjustmy" target="_blank">
           <img id="linkedinIcon" type="image/svg+xml" src="../assets/linkedin-icon.svg" />
         </a>
-        <a class="ml-2" href="https://github.com/Isjustmy" target="_blank">
+        <a class="ml-4" href="https://github.com/Isjustmy" target="_blank">
           <img id="githubIcon" type="image/svg+xml" src="../assets/github-icon.svg" />
         </a>
       </div>
@@ -42,10 +42,17 @@ export default {
 }
 
 #linkedinIcon {
-  filter: invert(1);
+  filter: brightness(0) invert(1);
+  width: 22px;
 }
 
 #githubIcon {
   filter: invert(1);
+  width: 22px;
+}
+
+.icon-normalize {
+  width: 32px;
+  height: 32px;
 }
 </style>
